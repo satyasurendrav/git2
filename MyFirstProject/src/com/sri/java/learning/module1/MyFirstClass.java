eb7d7f26a442f13b698ab9e08d38dfdd2a535aea
satyasurendrav <satya.suren@gmail.com>
2018-07-14 17:25:56 +0530

    some_message for MyFirstProject

---
+++ MyFirstProject/src/com/sri/java/learning/module1/MyFirstClass.java
@@ -0,0 +1,24 @@
+package com.sri.java.learning.module1;
+
+public class MyFirstClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi... This is my first program on linux machine");
+		int a=4;
+		int c=5;
+		System.out.println("Sum of a and c");
+		System.out.println(a+c);
+		System.out.println("Subtraction of a and c");
+		System.out.println(c-a);
+		System.out.println("Multiplication of a and c");
+		System.out.println(a*c);
+		
+		System.out.println("-----------This time starts now...");
+		int myVariable=4;
+		System.out.println(myVariable);
+		int asd=5;
+		System.out.println(asd);
+	}
+
+}
